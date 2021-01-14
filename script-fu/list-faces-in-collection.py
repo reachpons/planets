18ced80e098da08009676424a7fa0c1c513b6dd0
=ian Bunney <ianbunney@gmail.com>
2021-01-14 14:08:38 +0800

    script to extract Rekgnition Collection data

---
+++ script-fu/list-faces-in-collection.py
@@ -0,0 +1,37 @@
+
+import json
+import boto3
+from datetime import datetime
+
+def list_faces_in_collection(collection_id):
+    all={}    
+    maxResults=4096
+    client=boto3.client('rekognition')
+    
+    response=client.list_faces(CollectionId=collection_id,
+                               MaxResults=maxResults)
+    while response:
+
+        faces=response['Faces']
+        for face in faces:
+            all[face['ExternalImageId']]=face['FaceId']
+        
+        if 'NextToken' not in response: break           
+        response=client.list_faces(CollectionId=collection_id,
+                                    MaxResults=maxResults,
+                                    NextToken=response['NextToken'])     
+       
+    return all   
+
+def main():
+
+    print(datetime.utcnow())
+    collection_id='large-rekognition-collection'
+
+    saps=list_faces_in_collection(collection_id)
+    print( "faces count: {}".format(len(saps)) )
+
+    print(datetime.utcnow())
+
+if __name__ == "__main__":
+    main()
