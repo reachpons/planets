15a9f9d3bb54d3232dbb8b3552ab289eba7e6bb9
=ian Bunney <ianbunney@gmail.com>
2021-02-01 15:54:08 +0800

    added CORS headers to Lambda

--- handlers/alcolizer-get-photo-signedUrl.py
+++ handlers/alcolizer-get-photo-signedUrl.py
@@ -67,6 +67,11 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
+        'headers': { 
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': '*'
+        },
         'body': json.dumps({ 'event': eventId,
                             'urls' : urls 
                             } )
